272326a6562660dc210e3a186d48e64a6edee8ec
anniemon <seojung19@gmail.com>
2021-10-11 23:10:51 +0900

    config.js에 port넘버string으로 수정가

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     password: process.env.DATABASE_PASSWORD,
     database: 'todolist',
     host: 'codegangster-todolist.c4tretnl6cyc.ap-northeast-2.rds.amazonaws.com',
-    port: 13306,
+    port: '13306',
     dialect: 'mysql'
   },
   test: {
