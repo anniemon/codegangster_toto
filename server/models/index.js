332e69920e35a73a0843a5f85b9e4a2afe7ab16c
Hendrix1995 <hgud55@naver.com>
2021-10-11 14:46:44 +0900

    models index.js 포트 번호 443 변경

--- server/models/index.js
+++ server/models/index.js
@@ -14,29 +14,33 @@ if (config.use_env_variable) {
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, {
     host: 'codegangster-todolist.c4tretnl6cyc.ap-northeast-2.rds.amazonaws.com',
-    port: 80,
+    port: 443,
     logging: console.log,
     maxConcurrentQueries: 100,
     dialect: 'mysql',
     dialectOptions: {
-        ssl:'Amazon RDS'
+      ssl: 'Amazon RDS'
     },
-    pool: { maxConnections: 5, maxIdleTime: 30},
+    pool: { maxConnections: 5, maxIdleTime: 30 },
     language: 'en'
-  })
+  });
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
